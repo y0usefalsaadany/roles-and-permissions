a0be5a6793bc20277eb6a9f1f3ab9512fbbed614
unknown <yousefalsaadany5@gmail.com>
2024-03-23 12:43:28 +0200

    policies

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,30 +8,52 @@ use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Post::class);
+    }
+    public function index()
+    {
+        // if (request()->user()->cannot('viewAny', Post::class)) {
+        //     return response()->json([
+        //                 "message" => "forbidden"
+        //             ], 403);
+        //     // $this->authorize('viewAny', Post::class);
+        // }
+        return response()->json([
+            "data" => Post::all()
+        ]);
+    }
     public function store()
     {
-        if (! Gate::allows('create_post')) {
-            return response()->json([
-            "message" => "forbidden"
-            ],403);
-        }
+        // $this->authorize('create', Post::class);
+
+        // if (!Gate::allows('create_post')) {
+        //     return response()->json([
+        //         "message" => "forbidden"
+        //     ], 403);
+        // }
         $post = Post::create(request()->all());
         return response()->json([
             "message" => "created"
         ]);
     }
 
-    public function update($id)
+    public function update(Post $post)
     {
-        $post = Post::find($id);
-        if (! Gate::allows('update_post',$post)) {
-            return response()->json([
-            "message" => "forbidden"
-            ],403);
-        }
+        // if (!Gate::allows('update_post', $post)) {
+        //     return response()->json([
+        //         "message" => "forbidden"
+        //     ], 403);
+        // }
+        // $this->authorize('update', $post);
         $post->update(request()->all());
         return response()->json([
             "message" => "updated"
         ]);
+    }
+    public function destroy(Post $post)
+    {
+        $post->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
